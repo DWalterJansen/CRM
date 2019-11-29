f541245cd72bd6745d12e9cf3dff068d59039944
David <Jansen>
2019-11-29 13:53:09 -0300

    Novos Usuários

--- bd/SQL - CRM.sql
+++ bd/SQL - CRM.sql
@@ -22,7 +22,7 @@ add constraint accepted_types check (tipo = 'Client' or tipo = 'Admin');
 -- Inserção no Banco
 insert into usuarios
 (nome, login, email, senha, tipo)
-values ('Paulo Borges', 'paulo', 'paulo.com', encode(sha384('paulo'), 'hex'), 'Admin');
+values ('Paulo Borges', 'paulo', 'paulo@gmail.com', encode(sha384('paulo'), 'hex'), 'Admin');
 
 insert into usuarios
 (nome, login, email, senha, tipo)
@@ -32,6 +32,32 @@ insert into usuarios
 (nome, login, email, senha, tipo)
 values ('David Jansen', 'david', 'david@gmail.com', encode(sha384('david'), 'hex'), 'Admin');
 
+insert into usuarios
+(nome, login, email, senha, tipo)
+values ('Marianna Leandra', 'maremaremare', 'mare@gmail.com', encode(sha384('lagartixa'), 'hex'), 'Admin');
 
+insert into usuarios
+(nome, login, email, senha, tipo)
+values ('Nayara Thaiza', 'nayara', 'nayara@gmail.com', encode(sha384('ornitorrinco'), 'hex'), 'Admin');
+
+insert into usuarios
+(nome, login, email, senha, tipo)
+values ('Vinicius Rodrigues', 'vinicius', 'vinicius@gmail.com', encode(sha384('vinicius'), 'hex'), 'Admin');
+
+insert into usuarios
+(nome, login, email, senha, tipo)
+values ('Roberta Rasovit', 'robertinha', 'roberta@gmail.com', encode(sha384('rasovit'), 'hex'), 'Admin');
+
+insert into usuarios
+(nome, login, email, senha, tipo)
+values ('Cliente Um', 'cliente1', 'cliente1@gmail.com', encode(sha384('cliente1'), 'hex'), 'Client');
+
+insert into usuarios
+(nome, login, email, senha, tipo)
+values ('Cliente Dois', 'cliente2', 'cliente2@gmail.com', encode(sha384('cliente2'), 'hex'), 'Client');
+
+insert into usuarios
+(nome, login, email, senha, tipo)
+values ('Cliente Tres', 'cliente3', 'cliente3@gmail.com', encode(sha384('cliente3'), 'hex'), 'Client');
 -- Visualizar tuplas completas
 select * from usuarios;
